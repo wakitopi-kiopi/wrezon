309ed833e6951b03f6ffca0535234a190e352b4a
wakitopi kitopi <wakitopikitopi214@gmail.com>
2026-08-29 01:03:22 +0200

    update changes

--- Frontwrezon/pc.js
+++ Frontwrezon/pc.js
@@ -1016,7 +1016,7 @@ export function livechatsession() {
 
 
 
-            //resetSilenceTimer();
+            resetSilenceTimer();
         }
 
         // --- TURN OFF: INTENTIONAL MANUAL CLICK ONLY ---
